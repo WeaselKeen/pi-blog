ea9e419ef010b4d9e1ee647fc6ff46d08c4c9767
Huan <liuhuan2472014@gmail.com>
2026-02-05 01:19:34 +0800

    Initial commit

---
+++ src/consts.ts
@@ -0,0 +1,5 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'π 的无限可能';
+export const SITE_DESCRIPTION = '探索、思考、记录。π 的个人自留地。';
